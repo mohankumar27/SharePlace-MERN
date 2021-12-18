de9b39c668bddb1cf2fe5e599f40fa3b0c38c78e
Mohan Kumar Sagadevan <mohankumar27197@gmail.com>
2021-12-18 12:00:33 +0530

    comment old cors logic

--- nodejs_backend/app.js
+++ nodejs_backend/app.js
@@ -24,15 +24,15 @@ app.use((req, res, next) => {
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 //To avoid blocked by CORS policy error
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+//   next();
+// });
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", userRoutes);
